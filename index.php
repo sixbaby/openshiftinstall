57237e6a1aaac4b6d96eb2171811767dbebda1de
sixbaby <18325004564@163.com>
2016-05-13 23:25:06 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>你的空间已经成功开通!</title>
+<meta name="viewport" content="width=device-width">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<!-- 新 Bootstrap 核心 CSS 文件 -->
+<link href="https://cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- 可选的Bootstrap主题文件（一般不使用） -->
+<script src="https:////cdn.bootcss.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"></script>
+
+<!-- jQuery文件。务必在bootstrap.min.js 之前引入 -->
+<script src="https://cdn.bootcss.com/jquery/2.2.3/jquery.min.js"></script>
+
+<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
+<script src="https://cdn.bootcss.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="alert alert-success">成功！你的openshift空间已经开通好了，再进行一些配置就可以完美使用了!</div>
+<div class="container">
+   <div class="jumbotron">
+      <h1>你的空间已经成功开通！</h1>
+      <p>是不是还在开通的成功中沉浸？很抱歉(*´▽｀)ノノ我们工作还没有做完!<br>下一步是安装在线文件管理，准备好没？点击下一步开始吧!*(๑• ₃ •๑)</p>
+      <p> <a href=fileinstall.php>下一步</a>
+      </p>
+   </div>
+</div>
+</div>
+<div class="well well-sm"><center><a href=http://bbs.mkbulo.com>陌空资源部落</a>提供技术支持！</center></div>
+</body>
+</html>
